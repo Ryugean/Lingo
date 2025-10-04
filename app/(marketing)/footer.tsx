1c3ae25c8413c9290afbc743f67618f5943af2fb
Nayel Abed Razi <mugwurp@gmail.com>
2025-10-04 08:53:35 +0530

    Adding SIdebar Component

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size='lg' variant='ghost'>
+          <Image 
+            src="/flags/hk-square-01-256.png" 
+            alt="Mandarine" 
+            height={32} 
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Mandarine
+        </Button>
+        <Button size='lg' variant='ghost'>
+          <Image 
+            src="/flags/es-square-01-256.png" 
+            alt="Spanish" 
+            height={32} 
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size='lg' variant='ghost'>
+          <Image 
+            src="/flags/bd-square-01-256.png" 
+            alt="Bengali" 
+            height={32} 
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Bengali
+        </Button>
+        <Button size='lg' variant='ghost'>
+          <Image 
+            src="/flags/gr-square-01-256.png" 
+            alt="Greece" 
+            height={32} 
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Greece
+        </Button>
+        <Button size='lg' variant='ghost'>
+          <Image 
+            src="/flags/cu-square-01-256.png" 
+            alt="Cuban" 
+            height={32} 
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Cuban
+        </Button>
+      </div>
     </footer>
   )
 }
